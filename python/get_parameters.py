e891db5aa630e97d92f25d44503ebcf2c4610818
jjl20011 <jjl20011@datasec.engr.uconn.edu>
2023-07-17 11:04:23 -0400

    modified get_parameters to write size of parameter tensors as well

--- python/get_parameters.py
+++ python/get_parameters.py
@@ -12,12 +12,15 @@ def load_model(file_handle="pretrained_torch_models/model_subj_9"):
 if __name__ == '__main__':
     # load the model and write all the parameters to the 
     # printed parameters file
+    results_file = input("Please enter the name of the file to write to")
     model_dict = load_model()
-    file_mode = "a"
+    file_mode = "w"
     import os
-    if not "parameters.txt" in os.listdir():
-        file_mode = "w"
+    #if not results_file in os.listdir():
+    #    file_mode = "w"
 
-    with open("parameters.txt", file_mode) as f:
+    with open(results_file, file_mode) as f:
         for name, value in model_dict:
             f.write(f"{name:20}:{value}\n")
+            f.write(f"{'size':20}:{value.size()}\n\n")
+            f.write(100*"="+"\n")
